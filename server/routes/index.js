d26d13108234e816a3cbeebf483717f06840f22b
Dimas Bagus Susilo <dimasbagussusilo@gmail.com>
2024-03-09 17:26:35 +0700

    refactioring ngasal

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,40 +5,80 @@ const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
 
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-router.get("/excel/aoa", cors(), (req, res) => {
-  const header = ["ID", "Nama", "Umur"];
-  const data = [
-    { id: 1, name: "Denny", age: 24 },
-    { id: 2, name: "Aditya", age: 25 },
-    { id: 3, name: "Pradipta", age: 26 },
-    { id: 4, name: "Ardhie", age: 27 },
-    { id: 5, name: "Putra", age: 28 },
-    { id: 6, name: "Prananta", age: 29 },
-  ];
-  const fileName = "AOA_XLS";
-  let wb = XLSX.utils.book_new();
-  wb.Props = {
-    Title: fileName,
-    Author: "Test",
-    CreatedDate: new Date(),
-  };
-  wb.SheetNames.push("Sheet 1");
-  let ws = XLSX.utils.json_to_sheet(data);
-  wb.Sheets["Sheet 1"] = ws;
+var mysql = require('mysql');
 
-  const downloadFolder = path.resolve(__dirname, "../downloads");
-  if (!fs.existsSync(downloadFolder)) {
-    fs.mkdirSync(downloadFolder);
-  }
-  try {
-    XLSX.writeFile(wb, `${downloadFolder}${path.sep}${fileName}.xls`);
-    res.download(`${downloadFolder}${path.sep}${fileName}.xls`);
-  } catch (e) {
-    console.log(e.message);
-    throw e;
-  }
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "password",
+    database: "direct-debit" // Add database name here
+
 });
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+function excelGenerator(fileName, author, sheetName, data) {
+    const wb = XLSX.utils.book_new();
+    wb.Props = {
+        Title: fileName,
+        Author: author,
+        CreatedDate: new Date(),
+    };
+    wb.SheetNames.push(sheetName);
+    wb.Sheets[sheetName] = XLSX.utils.json_to_sheet(data);
+
+    const downloadFolder = path.resolve(__dirname, "../downloads");
+    if (!fs.existsSync(downloadFolder)) {
+        fs.mkdirSync(downloadFolder);
+    }
+
+    XLSX.writeFile(wb, `${downloadFolder}${path.sep}${fileName}.xls`);
+
+    return `${downloadFolder}${path.sep}${fileName}.xls`
+}
+
+router.get("/", function (req, res, next) {
+    res.render("index", {title: "Express"});
+});
+router.get("/excel/payments", cors(), (req, res) => {
+    // const users = db.users.find({})
+    con.query('SELECT * FROM payments', function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+        const data = result.map(payment => {
+            return {
+                'ID': payment.id,
+                'Nomor Referensi': payment.reference_no,
+                'Token Pembayaran': payment.charge_token,
+            }
+        })
+
+        const newExcel = excelGenerator('payments-jan', 'dbs', 'January', data)
+
+        res.download(newExcel);
+    });
+});
+
+router.get("/excel/cards", cors(), (req, res) => {
+    con.query('SELECT * FROM cards', function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+        const data = result.map(card => {
+            return {
+                'ID': card.id,
+                'Card Data': card.card_data,
+            }
+        })
+
+        const newExcel = excelGenerator('cards-jan', 'dbs', 'January', data)
+
+        res.download(newExcel);
+    });
+});
+
 module.exports = router;
